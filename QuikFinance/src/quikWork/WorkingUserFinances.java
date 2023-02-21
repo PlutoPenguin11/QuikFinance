5120a0636875708f0ae8d3f13020ec0d6a7268e5
FilipCvet <124813015+FilipCvet@users.noreply.github.com>
2023-02-20 22:02:17 -0500

    Add files via upload
    these are the initial project files

---
+++ QuikFinance/src/quikWork/WorkingUserFinances.java
@@ -0,0 +1,13 @@
+package quikWork;
+import quikConfig.CategoryClass;
+//main classs
+public class WorkingUserFinances {
+
+	public static void main(String[] args) {
+		CategoryClass item1;
+		item1 = new CategoryClass("uhhh entertainment",10);
+		item1.printCategory();
+	}
+
+}
+
