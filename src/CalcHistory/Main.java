313ad1811c4f767f33f26d92104d3b4d0ac6ba9c
BananaSausage11 <124006402+PlutoPenguin11@users.noreply.github.com>
2023-02-21 14:13:33 -0500

    AddTransaction
    Adds the transaction class. Not complete, but at least you all will be able to see what I've got so far.

---
+++ src/CalcHistory/Main.java
@@ -0,0 +1,8 @@
+package CalcHistory;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/Ledger/CategoryClass.java
@@ -0,0 +1,25 @@
+package Ledger;
+
+//this is the class for creating categories for whatever you are buying !!!????
+public class CategoryClass {
+    private int itemPrice;
+    private String itemType = null;
+
+    public CategoryClass(String itemType, int itemPrice) {
+        this.itemPrice = itemPrice;
+        this.itemType = itemType;
+    }
+
+    public int getPrice() {
+        return this.itemPrice;
+    }
+
+    public String getType() {
+        return this.itemType;
+    }
+
+    public void printCategory() {
+        System.out.println(getPrice());
+        System.out.println(getType());
+    }
+}
